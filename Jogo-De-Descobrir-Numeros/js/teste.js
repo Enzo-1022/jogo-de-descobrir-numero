a3ab5c526dbc2f79f656e77a644109e184487928
Enzo Fernandes <enzofernandes560@gmail.com>
2023-11-01 20:00:34 -0300

    Iniciei os testes
    Criei o arquivo de testes e comecei o codigo

---
+++ Jogo-De-Descobrir-Numeros/js/teste.js
@@ -0,0 +1,8 @@
+/*
+Presiso gerar um numero aleatorio de 0 a 10
+pedir para o usuario tentar adivinhar esse numero 
+verificar se o numero chutado é igual ao numaro gerado pelo sistema 
+o usuario tem que ter quantas chances quiser
+*/
+
+var numGeradoPeloSitema = Math.floor(Math.random()*11)
