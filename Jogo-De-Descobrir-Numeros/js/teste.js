5c9ed7d72e5104427602d69a481364a6c40c18d0
Enzo Fernandes <enzofernandes560@gmail.com>
2023-11-01 21:54:08 -0300

    deu tudo errado
    deu tudo errado js do ceu! vc é mais dificil de entender do que tentar entender uma mina pqp

--- Jogo-De-Descobrir-Numeros/js/teste.js
+++ Jogo-De-Descobrir-Numeros/js/teste.js
@@ -4,5 +4,3 @@ pedir para o usuario tentar adivinhar esse numero
 verificar se o numero chutado é igual ao numaro gerado pelo sistema 
 o usuario tem que ter quantas chances quiser
 */
-
-var numGeradoPeloSitema = Math.floor(Math.random()*11)
