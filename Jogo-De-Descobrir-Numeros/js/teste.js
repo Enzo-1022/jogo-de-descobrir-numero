8496e7c203930a0718d473cf55c8340426ca241a
Enzo Fernandes <enzofernandes560@gmail.com>
2023-11-04 23:54:21 -0300

    terminando

--- Jogo-De-Descobrir-Numeros/js/teste.js
+++ Jogo-De-Descobrir-Numeros/js/teste.js
@@ -1,7 +1,7 @@
 const paragrafo01 = document.getElementById('paragrafo01')
 const paragrafo02 = document.getElementById('paragrafo02')
 const btnComecar = document.getElementById('btnComecar')
-
+const cores = ['red','blue','purple','brown']
 var numChutado;
 var numGerado;
 
@@ -9,30 +9,38 @@ btnComecar.addEventListener('click', function(){
     comecar()
 })
 
+//FUNÇÃO PARA TROCAR COR DO PARGRAFO
+function trocarCorPargrafo() {
+    var indeceAleatorio = Math.floor(Math.random()*cores.length)
+    paragrafo01.style.color = `${cores[indeceAleatorio]}`
+}
+
 //Função de verificação
 function verificar(numeroChutado, numeroGeradoPeloSistema) {
-    console.log('foi')
     if (numeroChutado != numeroGeradoPeloSistema) {
         if (numeroChutado > numeroGeradoPeloSistema) {
+            trocarCorPargrafo()
             paragrafo01.innerText = 'Errado!! Tente novamente você CHUTOU ALTO!!'
         } else {
              paragrafo01.innerText = 'Errado!! Tente novamente você CHUTOU BAIXO!!'
         }
     } else {
         paragrafo01.innerText = `VOCÊ ACERTOU!!!! O numero gerado foi ${numeroGeradoPeloSistema}`
-        paragrafo02.innerHTML = '<button type="button" onclick="comecar()">Jogar Novamente</button>'
+        paragrafo02.innerHTML = '<button type="button" onclick="comecar()" class="botoes-comecar-e-jogar-novamente">Jogar Novamente</button>'
     }
 }
 
 //Função para começar o game
 function comecar() {
     
+    //Limpa o paragrafo com os resultados 
+    paragrafo01.innerHTML = ''
+
     //Gera numero aleatorio de 0 ate 10
     numGerado = Math.floor(Math.random()*11)
-    console.log(numGerado)
 
     // Input para recolher o numero chutado pelo usuario
-    paragrafo02.innerHTML = '<input type="number" max="10" min="0" id="iptnNumeroChutado"> <button type="button" id="btnVerificar">Verificar</button>'
+    paragrafo02.innerHTML = '<input type="number" max="10" min="0" id="iptnNumeroChutado"><button type="button" id="btnVerificar">Verificar</button>'
     var iptnNumeroChutado = document.getElementById('iptnNumeroChutado')
 
     //verificação
